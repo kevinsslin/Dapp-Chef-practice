9b8350f051f39c099709c2ff9e988e3aea81e97d
kevinsslin <kk123750964@gmail.com>
2023-04-27 02:44:50 +0800

    answer23 update

--- Beginner/answer23.sol
+++ Beginner/answer23.sol
@@ -10,13 +10,8 @@ contract answer23 {
     
     function hashInOrder() public {
         //TODO: you should use ONE keccak256 function to hash the above constant variables in order (UINT -> INT -> CALLER ...), and assign the value to result.
-        result = keccak256(abi.encode(UINT, INT, CALLER, BYTES));
-        
-        // our output is  0x77d1397b6ebc367f3acecdc488a586167e4059f2a0c6de0d2fa530b109a2b4c5
-        // if you check the sample output, you will find that the result is not the same as the our output, and I'm not sure what happen...
-        // to sucesssfully pass the test, you should use the following code:
-        
-        // result = bytes32(0x0c9abc34bde31d70a3c9d76141f5d4a79113efa5cf807a4e0bd01ef60ef4d573);
+        // if you use abi.encode(...), you'll get wrong answer
+        result = keccak256(abi.encodePacked(UINT, INT, CALLER, BYTES));
     }
 
     function get_result() public view returns(bytes32) {
